0da3e3e9e1d164feaec7f1d59503d3784e2705b9
RaphaelWright <raphaelwright2003@gmail.com>
2022-08-02 21:14:55 +0000

    minor updates, created routes for other nav tabs, woroked on body background, and fixed title issues

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from multiprocessing.pool import ApplyResult
+from turtle import title
 from flask import Flask, render_template
 
 
@@ -7,6 +8,19 @@ app = Flask(__name__)
 #routings
 @app.route('/')
 def index():
-    return render_template("index.html")
+    title = "RTMS - Manage Tenants"
+    return render_template("index.html", title = title)
+
+
+@app.route('/about')
+def about():
+    title = "About RTMS"
+    return render_template('about.html', title = title)
+
+
+@app.route('/contact')
+def contact():
+    title = " RTMS - Contact"
+    return render_template("contact.html", title = title)
 
 
