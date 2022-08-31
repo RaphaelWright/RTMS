b2c514961e0ac5bdb687fb1b0d0fd2c3fc93a518
RaphaelWright <raphaelwright2003@gmail.com>
2022-08-31 16:59:19 +0000

    worked on connecting to database, and the signup page,

--- app.py
+++ app.py
@@ -1,23 +1,66 @@
-from multiprocessing.pool import ApplyResult
-from turtle import title
-from flask import Flask, render_template
+from flask import Flask,render_template,session,request
+from flask_mysqldb import MySQL
 
-
+ 
 app = Flask(__name__)
+app.config['SECRET_KEY']= 'RTMS'
 
-#routings
-@app.route('/')
-def index():
-    return render_template("index.html")
+#database config
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'root'
+app.config['MYSQL_DB'] = 'MyDB'
 
+mysql = MySQL(app)
 
-@app.route('/about')
+@app.route('/', methods=['GET'])
+def home():
+    # session['name'] = 'ralph'
+    name = request.form
+    print(name)
+    return render_template('index.html')
+
+    
+@app.route('/about', methods=['GET'])
 def about():
-    return render_template('about.html', title = title)
+    return render_template('about.html')
 
+@app.route('/login', methods=['POST'])
+def login():
+    data = request.form
+    session['password'] = data['password']
+    session['name'] = data['username']
+    return render_template('login.html')
 
 @app.route('/contact')
 def contact():
-    return render_template("contact.html", title = title)
+    return render_template("contact.html")
 
+@app.route('/signup',methods=['GET','POST'])
+def signup():
+    
+    return render_template("signup.html")
+
+@app.route('/login', methods= ['GET','POST'])
+def submit():
+    if request.method == "POST":
+        details = request.form
+        username = details['uname']
+        firstname = details['fname']
+        surname = details['surname']
+        email = details['pemail']
+        telephone = details['telephone']
+        password = details['pwd']
+        cur = mysql.connection.cursor()
+        cur.execute("INSERT INTO PERSONAL(Username,FirstName,Surname,Email,Telephone,Password) VALUES (%s, %s, %s, %s, %s, %s)", (username,firstname,surname,email,telephone,password))
+        mysql.connection.commit()
+        cur.close()
+        return 'success'
+
+    return render_template('login.html')
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
 
